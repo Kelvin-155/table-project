cb9561fb19a2e0d0afddaf783dca57d35bab293c
kelvin-155 <goody6538@gmail.com>
2025-11-04 08:05:12 -0800

    created a loop website

---
+++ javascript/loop.js
@@ -0,0 +1,35 @@
+
+//  for (let index = 1; index + 1  <=11; index++) {
+    
+//      alert(index)
+//  }
+
+
+
+// let index = 2;
+// while (index <= 20) {
+//     alert(index)
+//     index += 2
+// }
+
+
+// for (let index = 10; index - 1 >=0; index--) {
+//     alert(index);
+    
+// } 
+
+
+for (let index = 1; index + 1 <=101; index++ ) {
+
+alert(index)
+}
+    
+
+
+
+
+// let index =1;
+// do {
+//     alert(index);
+//     index += 2
+// } while (index  <=50);
